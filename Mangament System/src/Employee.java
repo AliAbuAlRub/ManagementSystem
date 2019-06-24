a3bf0abf01b8c6f2451fa74a1cfa4d88ea4a2f29
Diaa Alkhatib <Diyaaa-123@hotmail.com>
2019-06-24 02:28:42 -0900

    add qualification to employee class
    qualification array was added to employee class
    In addition to the addition of setters and getters for all the atributes in the employee class

--- Mangament System/src/Employee.java
+++ Mangament System/src/Employee.java
@@ -5,6 +5,38 @@ public class Employee {
 	private String name ; 
 	private int did ; 
 	private String email ;
+	private Qualifiqation[] qualifications;
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDid() {
+		return did;
+	}
+	public void setDid(int did) {
+		this.did = did;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Qualifiqation[] getQualifications() {
+		return qualifications;
+	}
+	public void setQualifications(Qualifiqation[] qualifications) {
+		this.qualifications = qualifications;
+	}
 	public Employee(int eid, String name, int did, String email) {
 		this.eid = eid;
 		this.name = name;
@@ -25,6 +57,7 @@ public class Employee {
 		}else {
 			return null ; 
 		}
+		
 		
 	}
 	
